040ab6f5f41f8610bd0a3eb6043cb60fa2257bf6
taher shoker <taher-elshoker@hotmail.com>
2019-07-23 19:49:20 +0200

    make it better

--- server.js
+++ server.js
@@ -23,16 +23,15 @@ app.use('/api/posts',require('./routes/api/posts'))
 app.use('/api/auth',require('./routes/api/auth'))
 
 
-// Serve static assets  in production
-if(process.env.NODE_ENV === 'production') {
-    // Set Static folder
-    app.use(express.static('client/build')) 
-
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-
-}
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 // set port 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
